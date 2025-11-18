b41a76383b0b2a5f95e1899fc258134f599e6450
edgardgpm <edgardmgp@hotmail.com>
2025-11-18 17:55:33 -0500

    Comentarios del Archivo .py

--- mercadoFC.py
+++ mercadoFC.py
@@ -1,21 +1,34 @@
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
-
-import matplotlib.pyplot as plt
-
+# Importación de librerías
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
+# Enrutado del archivo .csv
 file_location = os.path.dirname(os.path.abspath(__file__))
 
+# Creación del dataframe
 df = pd.read_csv(file_location + "/fc26.csv")
 
+# Descripción Estadística Básica
 print(df.head())
 print(df.info())
 print(df.describe())
 
+
+# ==== 4. Análisis Probabilístico ==== #
+
+# 4.1 Identificación de Distribuciones
+
+
+# 4.2 Cálculo de Probabilidades
+
+
+
+# Otros
+
+p_subida = df["¿Subió de precio?"].mean()
+print("P(Subió el precio) =", p_subida)
 
 plt.bar(df["Jugador"], df["Variación (%)"])
 plt.ylabel("Variación (%)")
@@ -24,13 +37,3 @@ plt.show()
 print(df[["Frecuencia en sobres", "Variación (%)"]].corr())
 
 
-
-X = df[["Media", "Frecuencia en sobres", "Precio S1","Precio S8"]]
-y = df["¿Subió de precio?"].map({"Sí":1,"No":0})
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-print(model.score(X_test, y_test))
